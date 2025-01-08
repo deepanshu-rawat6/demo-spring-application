f9c99719fa1b1007c752b20b3eef53c1db80e5b2
Deepanshu Rawat <83383871+deepanshu-rawat6@users.noreply.github.com>
2025-01-08 15:23:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,123 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     Name = "main-vpc"
-#   }
-# }
-
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.main.id
-
-#   tags = {
-#     Name = "main-igw"
-#   }
-# }
-
-# resource "aws_subnet" "public_subnet_1" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.1.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "us-east-1a"
-# }
-
-# resource "aws_subnet" "public_subnet_2" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.2.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "us-east-1b"
-# }
-
-# resource "aws_subnet" "public_subnet_3" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.3.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "us-east-1c"
-# }
-
-# resource "aws_route_table" "public_rt" {
-#   vpc_id = aws_vpc.main.id
-
-#   tags = {
-#     Name = "main-public-rt"
-#   }
-# }
-
-# resource "aws_route" "public_route" {
-#   route_table_id         = aws_route_table.public_rt.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.igw.id
-# }
-
-# resource "aws_route_table_association" "public_assoc_1" {
-#   subnet_id      = aws_subnet.public_subnet_1.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-
-# resource "aws_route_table_association" "public_assoc_2" {
-#   subnet_id      = aws_subnet.public_subnet_2.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-
-# resource "aws_route_table_association" "public_assoc_3" {
-#   subnet_id      = aws_subnet.public_subnet_3.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-
-# resource "aws_security_group" "jenkins_sg" {
-#   vpc_id = aws_vpc.main.id
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# resource "aws_iam_role" "s3_access_role" {
-#   name = "jenkins-s3-access-role"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = "sts:AssumeRole",
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+## IAM role for the instances to acccess S3 bucket
+# Role permissions required:
+# ECS_FullAccess
+# S3_Push Access (or maybe Full_Access)
+# Task Execution Role for loggs of ECS Tasks + Cloud Watch Access
 
 resource "aws_launch_template" "jenkins_lt" {
   name = "tf_jenkins-launch-template"
 
-  instance_type = "t2.xlarge"
-  image_id      = "ami-0f3617604457f3823"
+  instance_type = "t3.xlarge"
+  image_id      = "ami-0e2c8caa4b6378d8c"
 
   iam_instance_profile {
     arn = "arn:aws:iam::971422682872:instance-profile/ecs-jenkins-access"
@@ -132,7 +26,7 @@ resource "aws_launch_template" "jenkins_lt" {
   block_device_mappings {
     device_name = "/dev/sda1"
     ebs {
-      volume_size = 30
+      volume_size = 60
     }
   }
 
@@ -144,7 +38,7 @@ resource "aws_launch_template" "jenkins_lt" {
 
   ebs_optimized = "true"
 
-  # user_data = base64encode(file("./user_data.sh"))
+  user_data = base64encode(file("./user_data.sh"))
 }
 
 resource "aws_autoscaling_group" "jenkins_asg" {
@@ -168,6 +62,6 @@ output "asg_id" {
   value = aws_autoscaling_group.jenkins_asg.id
 }
 
-# output "iam_role_name" {
-#   value = aws_iam_role.s3_access_role.name
-# }
+output "lt_id" {
+  value = aws_launch_template.jenkins_lt.id
+}
