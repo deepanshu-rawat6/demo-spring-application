3c34556ac37ed497b1918ab877ea6c91e510a13b
Deepanshu Rawat <83383871+deepanshu-rawat6@users.noreply.github.com>
2025-01-11 01:08:59 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y openjdk-21-jdk maven
+# Update and upgrade
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y docker.io openjdk-21-jdk maven
+
+sudo systemctl enable docker
 
 # Setting paths for maven
 echo "export MAVEN_HOME=/usr/share/maven" | sudo tee /etc/profile.d/maven.sh
@@ -11,3 +14,26 @@ source /etc/profile.d/maven.sh
 
 # Adding the user to docker(for non-sudo access)
 sudo usermod -aG docker $USER
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Configure AWS ECS
+export AWS_REGION="us-east-1"
+export OS_PACKAGE="amd64.deb"
+
+# Installing ECS-Init Agent to register the EC2 Instances as Container Instances for ECS Cluster to run tasks
+curl -O https://s3.${AWS_REGION}.amazonaws.com/amazon-ecs-agent-${AWS_REGION}/amazon-ecs-init-latest.${OS_PACKAGE}
+sudo dpkg -i amazon-ecs-init-latest.${OS_PACKAGE}
+
+sudo sed -i '/\[Unit\]/a After=cloud-final.service' /lib/systemd/system/ecs.service
+echo "ECS_CLUSTER=<"CLUSTER_NAME">" | sudo tee /etc/ecs/ecs.config
+
+sudo systemctl enable ecs
+sudo systemctl daemon-reload
+
+sudo systemctl start docker
+sudo systemctl start ecs
+
+sudo systemctl reboot
